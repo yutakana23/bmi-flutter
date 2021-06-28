72eab463dfc807d8876cf87b9cd9dc1e3cebcd33
Yuta Imai <immoarii.27623@icloud.com>
2021-06-29 02:58:57 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/yutakana23/bmi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yutakana23.bmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith( //全体で行うことを設定としてはじめに定義
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
